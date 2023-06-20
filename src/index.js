d186e383e7e76f2188bde05145bf1b781d32f1fc
daemusin12 <engrleonelfelezario812>
2023-06-21 02:48:52 +0800

    GET Request

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const app = express();
 const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
+
+app.get('/groceries', (req, res) => {
+    res.send([
+        {
+            item: 'Milk',
+            quantity: 2,
+        },
+        {
+            item: 'Egg',
+            quantity: 12,
+        },
+        {
+            item: 'Tocino',
+            quantity: 1,
+        },
+    ]);
+});
