a3a1a07874ca951f509292d3fd1e7587391744ea
daemusin12 <engrleonelfelezario812>
2023-06-21 14:38:44 +0800

    POST Requests

--- src/index.js
+++ src/index.js
@@ -3,21 +3,31 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
+let groceriesList = [
+    {
+        item: 'Milk',
+        quantity: 2,
+    },
+    {
+        item: 'Egg',
+        quantity: 12,
+    },
+    {
+        item: 'Tocino',
+        quantity: 1,
+    },
+];
+
 app.get('/groceries', (req, res) => {
-    res.send([
-        {
-            item: 'Milk',
-            quantity: 2,
-        },
-        {
-            item: 'Egg',
-            quantity: 12,
-        },
-        {
-            item: 'Tocino',
-            quantity: 1,
-        },
-    ]);
+    res.send(groceriesList);
+});
+
+app.post('/groceries', (req, res) => {
+    console.log(req.body);
+    groceriesList.push(req.body)
+    res.send(201);
 });
