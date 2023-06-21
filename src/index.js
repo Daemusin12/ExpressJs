4df2604d2828696eb2034ecfcc95e6c55894e201
daemusin12 <engrleonelfelezario812>
2023-06-21 15:04:36 +0800

    Middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const PORT = 3001;
 
 app.use(express.json());
 
+app.use((req, res, next) =>{
+    console.log(`${req.method}:${req.url}`);
+    next();
+});
+
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
 
 let groceriesList = [
