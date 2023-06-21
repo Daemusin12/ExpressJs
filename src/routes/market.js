63d130ef48f69efd320060841131ed9cded6674d
daemusin12 <engrleonelfelezario812>
2023-06-21 17:50:43 +0800

    Query Parameters

--- src/routes/market.js
+++ src/routes/market.js
@@ -4,14 +4,24 @@ const router = Router();
 
 let superMarkets = [
     {
+        id: 1,
         store: 'Whole Foods',
+        miles: .5,
     },
     {
+        id: 2,
         store: 'Trader Joes',
+        miles: 1.3,
     }
 ];
 
 router.get('/', (req, res) => {
+    const { miles } = req.query;
+    const parsedMiles = parseFloat(miles);
+    if (!isNaN(parsedMiles)) {
+        const filteredStores = superMarkets.filter((s) => s.miles <= parsedMiles);
+        res.send(filteredStores);
+    } else res.send(superMarkets);
     res.send(superMarkets);
 });
 
